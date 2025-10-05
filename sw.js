53d1e46b7992c25912fe73eb2b9fc57de3b6ce8c
CoachBJ <coachbiggiejames@gmail.com>
2025-10-05 08:16:10 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,31 @@
-const CACHE = 'scoring-v16';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png',
-  './icon-180.png'
-];
+const VERSION = 'v4'; // bump this after any deploy
+const SCOPE = new URL(self.registration.scope).pathname; // "/CCS_GameManager/"
+const PRECACHE = [
+  '', 'index.html', 'style.css', 'app.js',
+  'manifest.webmanifest', 'icon-192.png', 'icon-512.png'
+].map(p => new URL(p, SCOPE).toString());
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(PRECACHE)));
+  self.skipWaiting();
 });
-
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+      Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(resp => {
+        const clone = resp.clone();
+        caches.open(VERSION).then(cache => cache.put(e.request, clone));
+        return resp;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
   );
 });
